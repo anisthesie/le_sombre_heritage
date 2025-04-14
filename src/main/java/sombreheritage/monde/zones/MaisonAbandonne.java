ed94219c72dab3ad32933ee7b81a1f3da9c727ae
Anis <lowdinggg@gmail.com>
2025-04-14 19:53:03 +0100

    MaisonAbandonne WIP

--- src/main/java/sombreheritage/monde/zones/MaisonAbandonne.java
+++ src/main/java/sombreheritage/monde/zones/MaisonAbandonne.java
@@ -11,6 +11,29 @@ public class MaisonAbandonne extends Zone {
     }
 
     @Override
+    public void entrer() {
+        etapeConversation = 0;
+
+        if (!dejaVisite()) {
+            getJeu().afficher("Une demeure lézardée, aux vitres brisées, laisse filtrer un vent froid.");
+            getJeu().afficher("Des livres et parchemins sont éparpillés sur le sol poussiéreux.");
+            getJeu().afficher("Un laboratoire au sous-sol recèle des fioles brisées, d’étranges instruments.");
+            getJeu().afficher("On devine qu’une recherche malsaine ou interdite s’y est déroulée.");
+        }
+        getJeu().afficher("Vous êtes directement affronté par une créature aquatique.");
+        getJeu().afficher("C'est un être amphibien qui rôde sous la surface et dévore tout ce qui s'approche.");
+        getJeu().afficher("Vous devez l'affronter.");
+        getJeu().afficher("Vos choix sont : ");
+        getJeu().afficher("     1) Lui donner une part de sang.");
+        getJeu().afficher("     2) Sacrifier un objet précieux.");
+        getJeu().afficher("     3) L'attaquer frontalement.");
+        getJeu().afficher("Taper 1, 2 ou 3 pour choisir.");
+        getJeu().afficher("");
+
+        this.dejaVisite = true;
+    }
+
+    @Override
     public void traiterCommande(String commande) {
 
     }
